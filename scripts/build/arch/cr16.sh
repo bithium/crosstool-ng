e770eaffd0adc2de0c489a49d4e1443dd6c5c6bb
Filipe Alves <filipe.alves@bithium.com>
2015-03-09 16:27:18 +0000

    Initial support for CR16C.

---
+++ scripts/build/arch/cr16.sh
@@ -0,0 +1,15 @@
+# Compute CR16C-specific values
+
+CT_DoArchTupleValues() {
+    # gcc ./configure flags
+
+    CT_TARGET_ARCH=cr16-elf
+
+    CT_ARCH_WITH_ARCH=
+    CT_ARCH_WITH_ABI=
+    CT_ARCH_WITH_CPU=
+    CT_ARCH_WITH_TUNE=
+    CT_ARCH_WITH_FPU=
+    CT_ARCH_WITH_FLOAT=
+    CT_TARGET_SYS=
+}
